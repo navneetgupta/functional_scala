ef81c0e98c3912f248bc895b419f9a76aec6c408
Navneet <navneet.gupta.iitbhu@gmail.com>
2018-09-24 10:20:30 +0400

    Equal Examples

---
+++ src/main/scala/com/navneetgupta/scalaz/polymorphism/AdHocPoly.scala
@@ -0,0 +1,25 @@
+package com.navneetgupta.scalaz.polymorphism
+
+import scalaz._
+import Scalaz._
+
+case class Order(quantity: Double, amount: Double)
+object Order {
+  implicit val orderEq: Equal[Order] =
+    Equal[Order] { (o1, o2) =>
+      (o1.quantity == o2.quantity) // Here we can modify the custom Comparator. Instead of default whihc compare whole Object
+    }
+}
+
+object EqualApp extends App {
+  val o1 = Order(10.22, 23.2)
+  val o2 = Order(10.22, 23.232)
+  val o3 = Order(2.23, 23.232)
+
+  import Order._
+
+  println(o1 === o2)
+  println(o1 === o3)
+  println(o1 =/= o2)
+  println(o1 =/= o3)
+}
---
+++ src/main/scala/com/navneetgupta/scalaz/polymorphism/SubTypePoly.scala
@@ -0,0 +1,12 @@
+package com.navneetgupta.scalaz.polymorphism
+
+import scalaz._
+import Scalaz._
+
+class SubTypePoly {
+
+}
+
+//class Student extends Equal[Student] {
+//  def equal(a2: Student): Student = ???
+//}
