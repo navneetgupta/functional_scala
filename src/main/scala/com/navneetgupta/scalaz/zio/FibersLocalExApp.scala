39dcc95b6469ba5abf46d2214df2d1209c39900a
Navneet <navneet.gupta.iitbhu@gmail.com>
2018-12-20 14:04:47 +0400

    changes

---
+++ src/main/scala/com/navneetgupta/scalaz/zio/FibersLocalExApp.scala
@@ -0,0 +1,29 @@
+package com.navneetgupta.scalaz.zio
+
+import scalaz.zio.FiberLocal
+
+object FibersLocalExApp extends App {
+
+  val a  = for {
+    local <- FiberLocal.make[Int]
+    _ <- local.set(3)
+    v <- local.get
+    _ <- local.empty
+  } yield v == Some(3)
+
+  // always get value after setting it up other wise it would lead to memory leak
+  // use locally instead to avoid the memory Leakage
+
+  
+
+
+  val b = for {
+    local <- FiberLocal.make[Int]
+    v = local.locally(10)(local.get)
+  } yield v == Some(3)
+
+
+  println(a)
+  println(b)
+
+}
