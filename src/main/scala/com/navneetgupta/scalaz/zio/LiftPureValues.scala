f868af6f3f3a586113f562a597d09f9a0aa7fcba
Navneet <navneet.gupta.iitbhu@gmail.com>
2018-12-29 13:55:30 +0400

    changes

--- src/main/scala/com/navneetgupta/scalaz/zio/LiftPureValues.scala
+++ src/main/scala/com/navneetgupta/scalaz/zio/LiftPureValues.scala
@@ -1,12 +1,14 @@
 package com.navneetgupta.scalaz.zio
 
-import scalaz.zio.IO
+import scalaz.zio.{IO, RTS}
 import java.io.File
 import java.io.IOException
+import scalaz.zio.console._
 
 import org.apache.commons.io.FileUtils
 
-object LiftPureValues extends App {
+object LiftPureValues extends App{
+  val rts = new RTS{}
   val p: IO[Nothing, String] = IO.point("Hello World!!!!")
 
   //  The constructor uses non-strict evaluation, so the parameter will not be evaluated until
@@ -16,8 +18,7 @@ object LiftPureValues extends App {
   //  Alternately, you can use the IO.now constructor to perform strict evaluation of the value:
 
   val p1 = IO.now("Hello World Strictly!!!!!!!")
-  println(p)
-  println(p1)
+
 
 
   //  use the sync method of IO to import effectful synchronous code into your purely functional program:
@@ -64,5 +65,19 @@ object LiftPureValues extends App {
     added <- IO.point(list.map(_ + 1))
   } yield added
 
+  rts.unsafeRun( for {
+    a <- p
+    _ <- putStrLn(a)
+    b <- p1
+    _ <- putStrLn(b)
+    c <- z1
+    _ <- putStrLn(c.toString)
+//    d <- z2
+//    _ <- putStrLn(d)
+    e <- z3
+    _ <- putStrLn(e.toString)
+    f <- z4
+    _ <- putStrLn(f.toString)
+  }yield ())
 
 }
