cc679cfc559dcaecda450c4d0ff4beba5d769c25
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-01-21 09:59:49 +0400

    changes

--- src/main/scala/com/navneetgupta/freestyle/AlgebraApp.scala
+++ src/main/scala/com/navneetgupta/freestyle/AlgebraApp.scala
@@ -7,7 +7,7 @@ object AlgebraEx {
   case class User(name: String, email: String, id: Long)
 
   @free trait Users {
-    def get(id: Long): FS[User]
+    def get(id: Long): FS[Option[User]]
     def save(user: User): FS[User]
     def getAll(filter: String): FS[List[User]]
   }
@@ -17,7 +17,7 @@ object AlgebraEx {
 
     import cats.implicits._
 
-    def program(user: User): FS.Seq[User] =
+    def program(user: User): FS.Seq[Option[User]] =
       for {
         _         <- users.save(user)
         user      <- users.get(user.id)
@@ -26,5 +26,21 @@ object AlgebraEx {
 }
 
 object AlgebraExApp extends App {
-  
+  import AlgebraEx._
+  import cats.effect.IO
+  import cats.implicits._
+
+  var usersList = List(User("Navneet Gupta", "nkumar@adp.com", 1), User("Gupta", "gupta@adp.com", 2), User("Navneet", "navneet@gmail.com",3))
+
+  implicit val UserHandler = new Users.Handler[IO] {
+    override protected def get(id: Long): IO[Option[User]] = IO{usersList.filter(_.id == id).get(0)}
+
+    override protected def getAll(filter: String):IO[List[User]] = IO {usersList}
+
+    override protected def save(user: User): IO[User] = IO{
+      usersList.filter(_.id == user.id) match {
+        case user::users => user
+        case Nil => usersList = user::usersList; user
+      }}
+  }
 }
