d428e690a4cd12ff2b7e874c8856e35dbef38b52
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-01-25 10:03:10 +0400

    changes

--- src/main/scala/com/navneetgupta/freestyle/TaglessFreeCombinedInterpreterEx.scala
+++ src/main/scala/com/navneetgupta/freestyle/TaglessFreeCombinedInterpreterEx.scala
@@ -1,26 +1,26 @@
 package com.navneetgupta.freestyle
 
-import cats.Monad
-import freestyle.free.logging._
+import freestyle.free.logging.LoggingM
 import freestyle.free._
 import cats.implicits._
 import freestyle.tagless.tagless
 
 object TaglessFreeCombinedInterpreterEx {
-  @tagless(true) trait Validation {
+
+  @tagless(true) trait Validation1 {
     def minSize(s: String, n: Int): FS[Boolean]
     def hasNumber(s: String): FS[Boolean]
   }
 
-  @tagless(true) trait Interaction {
+  @tagless(true) trait Interaction1 {
     def tell(msg: String): FS[Unit]
     def ask(prompt: String): FS[String]
   }
 
-  def taglessProgram[F[_]:Monad]
+  def taglessProgram[F[_]]
   (implicit loggingM : LoggingM[F],
-   validation : Validation.StackSafe[F],
-   interaction: Interaction.StackSafe[F]) = {
+   validation : Validation1.StackSafe[F],
+   interaction: Interaction1.StackSafe[F]) = {
 
     for {
       userInput <- interaction.ask("Give me something with at least 3 chars and a number on it")
@@ -37,24 +37,37 @@ object TaglessFreeCombinedInterpreterEx {
 object TaglessFreeCombinedInterpreterExApp extends App {
   import scala.util.Try
   import freestyle.free.implicits._
-  import TaglessFreeCombinedInterpreterEx._
+  import freestyle.free.loggingJVM.journal.implicits._
+  import cats.implicits._
 
-  implicit val ValidationIOHandler = new Validation.Handler[Try] {
+  implicit val Validation1Handler = new TaglessFreeCombinedInterpreterEx.Validation1.Handler[Try] {
     def minSize(s: String, n: Int) = Try {s.length >= n}
     def hasNumber(s: String) : Try[Boolean] = Try{s.exists(c => "0123456789".contains(c))}
   }
 
-  implicit val InteractionHandler = new Interaction.Handler[Try] {
+  implicit val Interaction1Handler = new TaglessFreeCombinedInterpreterEx.Interaction1.Handler[Try] {
     def tell(msg: String) = Try{println(msg)}
     def ask(prompt: String) = Try {"This Could be a User input 1"}
   }
 
-  @module trait MApp {
-        val interaction: TaglessFreeCombinedInterpreterEx.Interaction.StackSafe
-        val validation: TaglessFreeCombinedInterpreterEx.Validation.StackSafe
-        val loggingM: LoggingM
-      }
+  trait MMApp[FF$61325[_]] extends _root_.freestyle.free.internal.EffectLike[FF$61325] {
+    val interaction: TaglessFreeCombinedInterpreterEx.Interaction1.StackSafe[FF$61325]
+    val validation: TaglessFreeCombinedInterpreterEx.Validation1.StackSafe[FF$61325]
+    val loggingM: LoggingM[FF$61325]
+  }
 
-  import freestyle.free.loggingJVM.journal.implicits._
-  TaglessFreeCombinedInterpreterEx.taglessProgram[MApp.Op].interpret[Try]
+  object MMApp {
+    type OpTypes = _root_.iota.TListK.Op.Concat[_root_.iota.TListK.Op.Concat[TaglessFreeCombinedInterpreterEx.Interaction1.StackSafe.OpTypes, TaglessFreeCombinedInterpreterEx.Validation1.StackSafe.OpTypes], LoggingM.OpTypes]
+    type Op[AA$61330] = _root_.iota.CopK[OpTypes, AA$61330]
+
+    class To[GG$61329[_]](implicit val interaction: TaglessFreeCombinedInterpreterEx.Interaction1.StackSafe[GG$61329], val validation: TaglessFreeCombinedInterpreterEx.Validation1.StackSafe[GG$61329], val loggingM: LoggingM[GG$61329]) extends MMApp[GG$61329] {}
+
+    implicit def to[GG$61329[_]](implicit interaction: TaglessFreeCombinedInterpreterEx.Interaction1.StackSafe[GG$61329], validation: TaglessFreeCombinedInterpreterEx.Validation1.StackSafe[GG$61329], loggingM: LoggingM[GG$61329]): To[GG$61329] = new To[GG$61329]()
+
+    def apply[FF$61325[_]](implicit ev$61331: MMApp[FF$61325]): MMApp[FF$61325] = ev$61331
+
+    def instance(implicit ev: MMApp[Op]): MMApp[Op] = ev
+  }
+
+  TaglessFreeCombinedInterpreterEx.taglessProgram[MMApp.Op].interpret[Try]
 }
