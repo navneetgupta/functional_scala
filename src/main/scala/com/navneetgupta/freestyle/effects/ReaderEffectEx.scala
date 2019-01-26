acbf418af1d73c81d2b9bf190a3339cda348ce9a
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-01-26 13:34:48 +0400

    changes

---
+++ src/main/scala/com/navneetgupta/freestyle/effects/ReaderEffectEx.scala
@@ -0,0 +1,42 @@
+package com.navneetgupta.freestyle.effects
+
+import cats.data.Reader
+import freestyle.free._
+import freestyle.free.implicits._
+import freestyle.free.effects.reader
+
+object ReaderEffectEx extends App {
+  // reader effect allows obtaining values from the environment.
+  // The initial seed for the environment value is provided at runtime interpretation.
+  // reader effect supports parametrization to any seed value type while remaining type safe throughout the program declaration.
+  //needs to be implicit evidence of cats.mtl.ApplicativeAsk[M, R] and Monad[M] for any runtime M[_] used in its interpretation due to the constraints placed by this effect. R
+
+  case class Config(n: Int)
+  type ConfigEnv[A] = Reader[Config, A]
+  val rd = reader[Config]
+
+  import cats.mtl.implicits._
+  import rd.implicits._
+
+  def programAsk[F[_]: rd.ReaderM] =
+    for {
+      _ <- FreeS.pure(1)
+      c <- rd.ReaderM[F].ask
+      _ <- FreeS.pure(1)
+    } yield c
+
+  println(programAsk[rd.ReaderM.Op].interpret[ConfigEnv].run(Config(n = 10)))
+
+
+  // reader allows extracting values of the environment and lifting them into the context of FreeS
+
+  def programReader[F[_]: rd.ReaderM] =
+    for {
+      a <- FreeS.pure(1)
+      b <- rd.ReaderM[F].reader(_.n)
+      c <- FreeS.pure(1)
+    } yield a + b + c
+  // programReader: [F[_]](implicit evidence$1: rd.ReaderM[F])cats.free.Free[[β$0$]cats.free.FreeApplicative[F,β$0$],Int]
+
+  println(programReader[rd.ReaderM.Op].interpret[ConfigEnv].run(Config(n = 1)))
+}
