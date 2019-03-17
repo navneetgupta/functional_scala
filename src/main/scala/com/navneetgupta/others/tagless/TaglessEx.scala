fd97ed1e0b157ffd705c8b6c9bec770cc38a2c7f
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-03-17 10:23:25 +0400

    changes

--- src/main/scala/com/navneetgupta/others/tagless/TaglessEx.scala
+++ src/main/scala/com/navneetgupta/others/tagless/TaglessEx.scala
@@ -1,8 +1,6 @@
 package com.navneetgupta.others.tagless
 
-import cats.Monad
 import cats.effect.IO
-import cats.implicits._
 
 object TaglessEx {
 
@@ -10,6 +8,19 @@ object TaglessEx {
     def putStrLn(msg: String): F[Unit]
 
     val getStrLn: F[String]
+  }
+
+  trait CMonad[F[_]] {
+    def pure[A](a: A): F[A]
+
+    def flatMap[A, B](value: F[A])(func: A => F[B]): F[B]
+
+    def map[A, B](value: F[A])(func: A => B): F[B] =
+      flatMap(value)(x => pure(func(x)))
+  }
+
+  object CMonad {
+    def apply[F[_]](implicit F: CMonad[F]): CMonad[F] = F
   }
 
   object Console {
@@ -22,7 +33,12 @@ object TaglessEx {
     override val getStrLn: IO[String] = IO(scala.io.StdIn.readLine)
   }
 
-  def program[F[_] : Console : Monad]: F[String] =
+  implicit class CMoandSyntax[F[_], A](fa: F[A]) {
+    def map[B](ab: A => B)(implicit P: CMonad[F]): F[B] = P.map(fa)(ab)
+    def flatMap[B](ab: A => F[B])(implicit P: CMonad[F]): F[B] = P.flatMap(fa)(ab)
+  }
+
+  def program[F[_] : Console : CMonad]: F[String] =
     for {
       _ <- Console[F].putStrLn("Good morning, what's your name?")
       name <- Console[F].getStrLn
@@ -31,12 +47,18 @@ object TaglessEx {
 
 }
 
-
 object Program1 extends App {
 
   import TaglessEx._
 
+  class IOCMonad extends CMonad[IO] {
+    override def pure[A](a: A): IO[A] = IO.pure(a)
+
+    override def flatMap[A, B](value: IO[A])(func: A => IO[B]): IO[B] = value.flatMap(func)
+  }
+
   implicit val console: Console[IO] = new ConsoleIO
+  implicit val iOCMonad: CMonad[IO] = new IOCMonad
 
   program.unsafeRunSync()
 }
@@ -52,15 +74,18 @@ object Program1Test extends App {
     def showResults = output.reverse.mkString("\n")
   }
 
+  case class TestIO[A](run: TestData => (TestData, A)) { self =>
+    def map[B](ab: A => B): TestIO[B] = TestIO(t => self.run(t) match {
+      case (t, a) => (t, ab(a))
+    })
 
-  case class TestIO[A](run: TestData => (TestData, A)) { s =>
-    def map[B](f: A => B): TestIO[B] = flatMap(a => TestIO.value(f(a)))
-    def flatMap[B](f: A => TestIO[B]): TestIO[B] =
-      TestIO(d =>
-        (s run d) match { case (d, a) => f(a) run d })
+    def flatMap[B](afb: A => TestIO[B]): TestIO[B] = TestIO(t => self.run(t) match {
+      case (t, a) => afb(a).run(t)
+    })
 
     def eval(t: TestData): TestData = run(t)._1
   }
+
   object TestIO {
     def value[A](a: => A): TestIO[A] = TestIO(d => (d, a))
   }
@@ -71,7 +96,21 @@ object Program1Test extends App {
     override val getStrLn: TestIO[String] = TestIO(t => t.getStrLn)
   }
 
-  implicit val consoleTestIO : Console[TestIO] = new ConsoleTestIO
+  class TestIOMonad extends CMonad[TestIO]{
+    override def flatMap[A, B](fa: TestIO[A])(f: A => TestIO[B]): TestIO[B] = fa.flatMap(f)
 
-  def programTestIO = program[TestIO]
+    override def map[A, B](fa: TestIO[A])(f: A => B): TestIO[B] = fa.map(f)
+    override def pure[A](x: A): TestIO[A] = TestIO.value(x)
+  }
+
+  implicit val consoleTestIO : Console[TestIO] = new ConsoleTestIO
+  implicit val testIOMonad: CMonad[TestIO] = new TestIOMonad
+
+
+  def programTestIO: TestIO[String] = program[TestIO]
+
+
+  val testData = TestData(List("Navneet Gupta"), Nil)
+  programTestIO.eval(testData).showResults
 }
+
